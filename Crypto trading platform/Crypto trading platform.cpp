07c96c6b16ba0e07dc94e8da7ab88d5defd82f8e
PramithaMJ <mihirangapramitha@gmail.com>
2023-06-09 17:22:13 +0530

    Add vstor stctures

--- Crypto trading platform/Crypto trading platform.cpp
+++ Crypto trading platform/Crypto trading platform.cpp
@@ -2,6 +2,7 @@
 
 
 #include <iostream>
+#include<vector>
 
 void prinMenu() {
 
@@ -83,12 +84,44 @@ void processUserOption(int userOption) {
 
 int main()
 {
-	while (true) //when type ctrl + c then exists the console (stops)
-	{
-		prinMenu();
-		int userOption = getUserOption();
-		processUserOption(userOption);
-	}
+	enum class OrderBookType { bid, ask };
+
+	//double price = 5319.450228;
+	//double amount = 0.00020075;
+	//std::string timestamp{ "2020/03/17 17:01:24.884492" };
+	//std::string product{ "BCT/USDT" };
+	//OrderBookType orderType = OrderBookType::ask;
+
+	std::vector<double> prices;
+	std::vector<double> amounts;
+	std::vector<std::string> timestamps;
+	std::vector<std::string> products;
+	std::vector<OrderBookType> orderTypes;
+
+	prices.push_back(500.01);
+	amounts.push_back(0.001);
+	timestamps.push_back("2020/03/17 17:01:24.884492");
+	products.push_back("BTC/USDT");
+	orderTypes.push_back(OrderBookType::bid);
+
+	prices.push_back(10.01);
+	amounts.push_back(0.001);
+	timestamps.push_back("2020/03/17 17:01:24.884492");
+	products.push_back("BTC/USDT");
+	orderTypes.push_back(OrderBookType::bid);
+
+	std::cout << "prices: " << prices[0] << std::endl;
+	std::cout << "prices: " << prices[1] << std::endl;
+
+
+
+
+	//while (true) //when type ctrl + c then exists the console (stops)
+	//{
+	//	prinMenu();
+	//	int userOption = getUserOption();
+	//	processUserOption(userOption);
+	//}
 
 	return 0;
 }
