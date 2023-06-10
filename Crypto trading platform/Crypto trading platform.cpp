f258343040e010a7f13060959b0f65e3e4e3d5ba
PramithaMJ <lpramithamj@gmail.com>
2023-06-10 10:01:52 +0530

    Create OrderBokkEntry Class

--- Crypto trading platform/Crypto trading platform.cpp
+++ Crypto trading platform/Crypto trading platform.cpp
@@ -3,6 +3,20 @@
 
 #include <iostream>
 #include<vector>
+#include"OrderBookEntry.h"
+
+enum class OrderBookType { bid, ask };
+
+class OrderBookEntry
+{
+public:
+	double price;
+	double amount;
+	std::string timestamp;
+	std::string product;
+	OrderBookType orderType;
+};
+
 
 void prinMenu() {
 
@@ -84,45 +98,53 @@ void processUserOption(int userOption) {
 
 int main()
 {
-	enum class OrderBookType { bid, ask };
-
+	
 	//double price = 5319.450228;
 	//double amount = 0.00020075;
 	//std::string timestamp{ "2020/03/17 17:01:24.884492" };
 	//std::string product{ "BCT/USDT" };
 	//OrderBookType orderType = OrderBookType::ask;
 
-	std::vector<double> prices;
-	std::vector<double> amounts;
-	std::vector<std::string> timestamps;
-	std::vector<std::string> products;
-	std::vector<OrderBookType> orderTypes;
+	//std::vector<double> prices;
+	//std::vector<double> amounts;
+	//std::vector<std::string> timestamps;
+	//std::vector<std::string> products;
+	//std::vector<OrderBookType> orderTypes;
 
-	prices.push_back(500.01);
-	amounts.push_back(0.001);
-	timestamps.push_back("2020/03/17 17:01:24.884492");
-	products.push_back("BTC/USDT");
-	orderTypes.push_back(OrderBookType::bid);
+	//prices.push_back(500.01);
+	//amounts.push_back(0.001);
+	//timestamps.push_back("2020/03/17 17:01:24.884492");
+	//products.push_back("BTC/USDT");
+	//orderTypes.push_back(OrderBookType::bid);
 
-	prices.push_back(10.01);
-	amounts.push_back(0.001);
-	timestamps.push_back("2020/03/17 17:01:24.884492");
-	products.push_back("BTC/USDT");
-	orderTypes.push_back(OrderBookType::bid);
+	//prices.push_back(10.01);
+	//amounts.push_back(0.001);
+	//timestamps.push_back("2020/03/17 17:01:24.884492");
+	//products.push_back("BTC/USDT");
+	//orderTypes.push_back(OrderBookType::bid);
 
-	std::cout << "prices: " << prices[0] << std::endl;
-	std::cout << "prices: " << prices[1] << std::endl;
-
-
+	//std::cout << "prices: " << prices[0] << std::endl;
+	//std::cout << "prices: " << prices[1] << std::endl;
 
 
-	while (true) //when type ctrl + c then exists the console (stops)
-	{
-		prinMenu();
-		int userOption = getUserOption();
-		processUserOption(userOption);
-	}
 
+
+	//while (true) //when type ctrl + c then exists the console (stops)
+	//{
+	//	prinMenu();
+	//	int userOption = getUserOption();
+	//	processUserOption(userOption);
+	//}
+
+	OrderBookEntry order1;
+
+	order1.price = 100000;
+	order1.amount = 0.002;
+	order1.timestamp = "2020/03/17 17:01:24.884492";
+	order1.product = "BTC/USDT";
+	order1.orderType = OrderBookType::bid;
+
+	std::cout << "The price is " << order1.price << std::endl;
 	return 0;
 }
 
---
+++ Crypto trading platform/OrderBookEntry.h
@@ -0,0 +1,14 @@
+//#pragma once
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//class OrderBookEntry
+//{
+//public:
+//	double price;
+//	double amount;
+//	string timestamp;
+//	string product;
+//	OrderBookType orderType;
+//};
