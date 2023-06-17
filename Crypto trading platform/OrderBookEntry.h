0c4147c8c7cf9faab79ac43156ff272fb171fcdb
unknown <lpramithamj@gmail.com>
2023-06-17 08:09:31 +0530

    Convert OrderBookEntry to header and cpp

--- Crypto trading platform/OrderBookEntry.h
+++ Crypto trading platform/OrderBookEntry.h
@@ -1,14 +1,21 @@
-//#pragma once
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//
-//class OrderBookEntry
-//{
-//public:
-//	double price;
-//	double amount;
-//	string timestamp;
-//	string product;
-//	OrderBookType orderType;
-//};
+#pragma once
+#include <iostream>
+#include <string>
+
+
+
+enum class OrderBookType { bid, ask };
+
+class OrderBookEntry
+{
+public:
+	//this way iniializing constructor is more efficient
+	OrderBookEntry(double _price, double _amount, std::string _timestamp, std::string _product, OrderBookType _orderType);
+
+	double price;
+	double amount;
+	std::string timestamp;
+	std::string product;
+	OrderBookType orderType;
+};
+
